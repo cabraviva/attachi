4e2fb6cf52ef570592afc21fa04af1ccd8beebd9
Green_Lab <greencoder001@outlook.com>
2023-05-11 20:11:36 +0200

    Started doing stuff

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,34 @@
 #! /usr/bin/env node
 import chalk from 'chalk'
-const args = process.argv.slice(2)
-console.log(chalk.cyan('Attachi running'))
+import express from 'express'
+import open from 'open'
+import { join } from 'path'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
+
+const app = express()
+const ui = express()
+const http = createServer(app)
+const io = new Server(http)
+
+app.use(express.static(join(__dirname, 'ui', 'lib')))
+ui.use(express.static(join(__dirname, 'ui', 'app')))
+
+let connected: string[] = []
+io.on('connection', (socket) => {
+    const pushindex = connected.push(socket.id) - 1
+    socket.on('a-exec-code', (to: string, code: string, cb: Function) => {
+        io.to(to).emit('l-exec-code', code, cb)
+    })
+    socket.on('disconnect', () => {
+        connected = connected.filter((_, i) => i !== pushindex)
+    })
+})
+
+http.listen(9608, () => {
+    console.log(chalk.cyan(`Attachi running on http://localhost:9608`))
+    ui.listen(9607, () => {
+        console.log(chalk.cyan(`UI running on http://localhost:9607`))
+        open('http://localhost:9607')
+    })
+})
